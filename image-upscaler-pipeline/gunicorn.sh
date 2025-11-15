65d58c2584fbe63ac85cce76932024c6e6713ef1
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-11-15 21:37:08 +0530

    temp removal for upscaler

--- image-upscaler-pipeline/gunicorn.sh
+++
@@ -1,5 +0,0 @@
-gunicorn -c gunicorn_config.py app:app & 
-sudo systemctl restart nginx & 
-docker build -t upscaler-service . &
-docker run -p 5000:5000 upscaler-service
-
--- image-upscaler-pipeline/gunicorn_config.py
+++
@@ -1,3 +0,0 @@
-workers = 4  # Number of parallel worker processes
-bind = "0.0.0.0:5000"
-timeout = 120  # Prevent timeout issues on large images
