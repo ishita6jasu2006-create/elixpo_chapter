5465587b935aeb5a6e0844da8293e4e35f5e7a14
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-01-02 15:25:39 +0530

    aded the vite bundler config

---
+++ art.elixpo/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: false,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  proxy : {
+    "/api": {
+      target: "http://localhost:5000",
+      changeOrigin: true,
+      secure: false,
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './JS'),
+    },
+  },
+});
